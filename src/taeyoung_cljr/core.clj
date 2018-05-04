d499afd2095119c52dfaeb160f67edf4487f2624
SophieMoon <tae.moon.young@gmail.com>
2018-05-04 21:47:36 +0900

    Make function

--- src/taeyoung_cljr/core.clj
+++ src/taeyoung_cljr/core.clj
@@ -1,7 +1,17 @@
 (ns taeyoung-cljr.core
   (:gen-class))
 
+(defn my-print
+  "프린트 함수"
+  [obj]
+  (println "my-print" obj)
+)
+
 (defn -main
   "lein run을 하면 이 함수가 실행됨."
   [& args]
-  (println "Hello, World!" args))
+  (my-print "아빠!!")
+  (my-print "again")
+  ;(println "Hello, World!" args)
+  )
+ 
